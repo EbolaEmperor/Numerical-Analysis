f2714a3de25f38e064c7bc1cfd83d20a7aa89df7
HUANG Wenchong <ebola@zju.edu.cn>
2022-12-05 20:13:31 +0800

    a little change

--- Project/src/ppForm.h
+++ Project/src/ppForm.h
@@ -6,7 +6,7 @@
 #include <cstring>
 #include <cmath>
 #include <limits>
-#include "Polynimial.h"
+#include "polynimial.h"
 #include "matrix.h"
 
 const double _epsL = 10 * std::numeric_limits<double>::epsilon();
